5e6d190f223d544a91cc57cc37390fed730cabe9
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-13 21:25:33 +0100

    4.1

--- 34-interfaces/src/main/java/it/unibo/encapsulation/interfaces/SimpleBankAccount.java
+++ 34-interfaces/src/main/java/it/unibo/encapsulation/interfaces/SimpleBankAccount.java
@@ -9,6 +9,9 @@ public class SimpleBankAccount implements BankAccount {
      * - static double ATM_TRANSACTION_FEE = 1: costo delle operazioni via ATM
      */
     private final int id;
+    private double balance;
+    private int transactions;
+    private final static double ATM_TRANSACTION_FEE = 1;
 
     /*
      * Creare un costruttore pubblico che prenda in ingresso un intero (ossia l'id
@@ -16,6 +19,8 @@ public class SimpleBankAccount implements BankAccount {
      */
     public SimpleBankAccount(final int id, final double balance) {
         this.id = id;
+        this.balance=balance;
+        this.transactions = 0;
     }
 
     /*
@@ -28,30 +33,59 @@ public class SimpleBankAccount implements BankAccount {
         return this.id;
     }
 
+    @Override
     public double getBalance() {
-        return 0.0;
+        return this.balance;
     }
 
+    @Override
     public int getTransactionsCount() {
-        return 0;
+        return this.transactions;
     }
 
+    public void setTransactions(final int transactions) {
+        this.transactions = transactions;
+    }
+
+    public void setBalance(final double balance) {
+        this.balance = balance;
+    }
+
+    public void incrementTransictions() {
+        this.setTransactions(this.getTransactionsCount()+1);
+    }
+
+    public void sumBalance(final double balance) {
+        this.setBalance(this.getBalance()+balance);
+    }
+
+    @Override
     public void deposit(final int id, final double amount) {
         /*
          * Incrementa il numero di transazioni e aggiunge amount al totale del
          * conto Nota: il deposito va a buon fine solo se l'id utente
          * corrisponde
          */
+        if(this.getid() == id) {
+            this.incrementTransictions();
+            this.sumBalance(amount);
+        }
     }
 
+    @Override
     public void withdraw(final int id, final double amount) {
         /*
          * Incrementa il numero di transazioni e rimuove amount al totale del
          * conto. Note: - Il conto puo' andare in rosso (ammontare negativo) -
          * Il prelievo va a buon fine solo se l'id utente corrisponde
          */
+        if(this.getid() == id) {
+            this.incrementTransictions();
+            this.sumBalance(-amount);
+        }
     }
 
+    @Override
     public void depositFromATM(final int id, final double amount) {
         /*
          * Incrementa il numero di transazioni e aggiunge amount al totale del
@@ -59,8 +93,13 @@ public class SimpleBankAccount implements BankAccount {
          * all'uso dell'ATM (bancomat) Nota: il deposito va a buon fine solo se
          * l'id utente corrisponde
          */
+        if(this.getid() == id) {
+            this.incrementTransictions();
+            this.sumBalance(amount-ATM_TRANSACTION_FEE);
+        }
     }
 
+    @Override
     public void withdrawFromATM(final int id, final double amount) {
         /*
          * Incrementa il numero di transazioni e rimuove amount + le spese
@@ -69,11 +108,19 @@ public class SimpleBankAccount implements BankAccount {
          * negativo) - Il prelievo va a buon fine solo se l'id utente
          * corrisponde
          */
+        if(this.getid() == id) {
+            this.incrementTransictions();
+            this.sumBalance(-(amount+ATM_TRANSACTION_FEE));
+        }
     }
 
+    @Override
     public void chargeManagementFees(final int id) {
         /*
          * Riduce il bilancio del conto di un ammontare pari alle spese di gestione
          */
+        if(this.getid() == id) {
+            this.sumBalance(-ATM_TRANSACTION_FEE);
+        }
     }
 }
--- 34-interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestSimpleBankAccount.java
+++ 34-interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestSimpleBankAccount.java
@@ -10,20 +10,25 @@ public class TestSimpleBankAccount {
 
     public static void main(final String[] args) {
         // 1) Creare l' AccountHolder relativo a Andrea Rossi con id 1
-
+        AccountHolder ah1 = new AccountHolder("Andrea", "Rossi", 1);
         // 2) Creare l' AccountHolder relativo a Alex Bianchi con id 2
-
+        AccountHolder ah2 = new AccountHolder("Alex", "Bianchi", 2);
         // 3) Creare i due SimpleBankAccount corrispondenti
-
+        BankAccount ba1 = new SimpleBankAccount(1, 10);
         // 4) Effettuare una serie di depositi e prelievi
-
+        BankAccount ba2 = new SimpleBankAccount(2, 20);
         /*
          * 5) Stampare a video l'ammontare dei due conti e verificare la
          * correttezza del risultato
          */
+        System.out.println(ba1.getBalance());
+        System.out.println(ba2.getBalance());
 
         // 6) Provare a prelevare fornendo un id utente sbagliato
+        ba1.withdraw(2, 100);
 
         // 7) Controllare nuovamente l'ammontare
+        System.out.println(ba1.getBalance());
+        System.out.println(ba2.getBalance());
     }
 }
